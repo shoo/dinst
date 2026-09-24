ee5b59f1fae0453692805e62d2be795f752e72b5
SHOO <zan77137@nifty.com>
2026-09-24 23:36:06 +0900

    test 1

--- tests/issue2.script.d
+++ tests/issue2.script.d
@@ -10,15 +10,24 @@ import dinst;
 
 void main()
 {
+import core.stdc.stdio; setbuf(cast(FILE*)core.stdc.stdio.stdout, null);
 	if (!isSupported)
 		return;
+writeln("TEST 1");
 	auto hook = setupHook!free;
+writeln("TEST 2");
 	static int pass;
 	hook.hook(delegate (void* ptr) {
+writeln("TEST 5");
 		pass = 1;
 		hook.callOrig(ptr);
+writeln("TEST 6");
 	});
+writeln("TEST 3");
 	auto ptr = malloc(100);
+writeln("TEST 4");
 	free(ptr);
+writeln("TEST 7");
 	assert(pass == 1);
+writeln("TEST 8");
 }
